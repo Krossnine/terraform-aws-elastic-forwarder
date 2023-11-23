b5074593dc7430444ad240f298603393c760346b
Steve Combat <scombat.42@gmail.com>
2023-11-23 09:52:31 +0100

    test(integration): Set localstack version to avoid integration tests fail

--- logs.tf
+++
@@ -1,6 +0,0 @@
-#tfsec:ignore:aws-cloudwatch-log-group-customer-key
-resource "aws_cloudwatch_log_group" "forwarder_log_group" {
-  name              = "/aws/lambda/${var.log_forwarder_lambda_function_name}"
-  retention_in_days = var.log_retention_in_days
-  tags              = var.default_tags
-}
